2c74739be570218a960a3dcce6a004e4660aae90
kiwi520 <roczhmg@163.com>
2017-12-11 11:15:29 +0800

    modify about views

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,32 +1,46 @@
 <template lang="html">
   <div>
     <div class="about">
+      <h1>About Website</h1>
+      <h3>本站迭代</h3>
+      <!--<p class="text">-->
+        <!--大家好，我是Cloud，坐标厦门，是一名90后coder（好吧，其实并没有“后”）,谦虚但喜欢装逼，热爱生活，热爱健身，热爱coding。立志全栈的页面仔。-->
+      <!--</p>-->
+      <!--<h3>技术栈</h3>-->
+      <ul class="list">
+        <li>2014年第一版使用WordPress+ALiYun</li>
+        <li>2015年第二版使用Laravel5.1+Slim+VueJs1.0+ALiYun</li>
+        <li>当前版使用BeeGo+VueJs2.0+DigitalOcean</li>
+      </ul>
+      <p  class="text">
+        从最初的使用开源博客系统,然后自己使用LNMP重构,现在又用GoLang重构...
+      </p>
+    </div>
+    <div class="about">
       <h1>About Me</h1>
       <h3>基本信息</h3>
       <p class="text">
-        大家好，我是Cloud，坐标厦门，是一名90后coder（好吧，其实并没有“后”）,谦虚但喜欢装逼，热爱生活，热爱健身，热爱coding。立志全栈的页面仔。
+        大家好，我是Kiwi，坐标济南，是一名90后coder,谦虚但喜欢装逼，热爱生活，热爱户外登山徒步等，热爱coding...
       </p>
       <h3>技术栈</h3>
       <ul class="list">
-        <li>前端三板斧</li>
-        <li>各主流CSS、JS库</li>
-        <li>Git、SVN</li>
-        <li>Vue.js</li>
-        <li>Node.js</li>
+        <li>Linux(Ubuntu Centos)</li>
+        <li>GoLang(标准库+BeeGo框架)</li>
+        <li>PHP(Laravel4+,Yii2+等主流组件框架)</li>
+        <li>Database(主要使用Mysql)</li>
+        <li>Cache(Redis,MemCached)</li>
+        <li>VCS(Git、SVN)</li>
+        <li>JS(VueJs1.0+,jQuery等)</li>
+        <li>Html(Bootstrap,Element UI)</li>
       </ul>
       <p  class="text">
-        前端基础扎实，掌握Jquery、Bootstrap、Vue(不限于)等主流JS、CSS库；
-        能完成各主流浏览器兼容、移动端响应式布局、安卓及IOS平台的Hybrid应用开发；
-        关注Web性能、体验(懒加载、预加载、缓存等)；
-        提倡前后端分离(Ajax)，
-        擅长基于Vue.js的SPA开发；
-        目前专注于node.js、MySQL、MongoDB等后端技术的学习。(反正吹牛逼又不用成本)
+        虽然可以手敲前端代码,目前还是主要做后端,倾向于全栈开发，励志做一个前后端通吃的kiwi...
       </p>
       <h3>联系我</h3>
       <ul class="list">
-        <li><a href="http://weibo.com/2808915224" target="_blank">新浪微博</a></li>
-        <li><a href="https://github.com/Lee-Cloud" target="_blank">Gitbub</a></li>
-        <li>QQ:595199568</li>
+        <li><a href="https://weibo.com/6126271354" target="_blank">新浪微博</a></li>
+        <li><a href="https://github.com/kiwi520" target="_blank">Gitbub</a></li>
+        <li>QQ:1102861547</li>
       </ul>
     </div>
   </div>
