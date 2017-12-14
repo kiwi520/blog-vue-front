621398791d1582e79ee8c095d6e6086018066977
kiwi520 <roczhmg@163.com>
2017-12-14 18:02:24 +0800

    modify markdown

--- src/views/post.vue
+++ src/views/post.vue
@@ -3,9 +3,8 @@
     <div class="post-cart">
       <h3 class="post-title">{{item.Title}}</h3>
       <p class="datetime">发表于：{{item.CreateTime}}</p>
-      <!--<div class="content">{{item.Content}}</div>-->
-      <div id="makeHtml" style="display: none">{{item.Content}}</div>
-      <div id="markdown"></div>
+      <div class="content" v-html="item.Content"></div>
+      <div id="content"></div>
       <scrollTop></scrollTop>
     </div>
   </div>
@@ -15,26 +14,6 @@
 <script scoped>
 import axios from 'axios'
 import scrollTop from '../components/scrollTop.vue'
-import { mavonEditor } from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
-import showdown from '/static/js/showdown.min.js'
-import prism from '/static/js/prism.js'
-
-
-
-var converter = new showdown.Converter(),
-  texts = $('#makeHtml').html();
-texts = texts.replace(/&lt;/g,"<");
-texts = texts.replace(/&gt;/g,">");
-texts = texts.replace(/&nbsp;/g," ");
-texts = texts.replace(/\n+/g,function(ns){
-  if(ns.length==1)
-    return '  '+ns
-  return ns
-});
-$('#markdown').html(converter.makeHtml(texts));
-
-
 export default {
   data () {
     return {
@@ -47,40 +26,24 @@ export default {
     if(article_id){
       axios.get('http://localhost:8889/v1/article/detail/'+article_id).then((response) => {
         this.item = response.data.data
+        console.log(response.data.data.Content)
       }).catch(e => {
         this.errors.push(e)
       })
     }
   },
-  computed: {},
+  computed: {
+
+  },
   mounted () {},
   methods: {
-    // 绑定@imgAdd event
-    $imgAdd(pos, $file){
-      alert("qweq")
-      // 第一步.将图片上传到服务器.
-      var formdata = new FormData();
-      formdata.append('image', $file);
-      axios({
-        url: 'http://localhost:8889/ueidtor',
-        method: 'post',
-        data: formdata,
-        headers: { 'Content-Type': 'multipart/form-data' },
-      }).then((url) => {
-        // 第二步.将返回的url替换到文本原位置![...](./0) -> ![...](url)
-        // $vm.$img2Url 详情见本页末尾
-        $vm.$img2Url(pos, url);
-      })
-    }
   },
   components: {
     scrollTop,
-    mavonEditor
   }
 }
 </script>
 <style lang="css" scoped>
-  @import '/static/css/prism.css';
   div.post-full{
     width: 960px;
     height: calc(100% - 250px);
@@ -107,6 +70,12 @@ export default {
     color: #888;
     font-size: 14px;
   }
+
+  div.post-cart .content{
+    width: auto;
+    height: 850px;
+  }
+
   div.post-cart div.content pre {
     background-color: #eee;
     overflow-x: scroll;
